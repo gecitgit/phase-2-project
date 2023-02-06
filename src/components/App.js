c446f228a375999c36cd1f491ea157f8ba77127e
+ <+>
2023-02-06 02:17:20 -0500

    Initial commit after stripping down create react app

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Beginning of the Phase 2 Proj</h1>
+      <h2>Here we go.</h2>
+    </div>
+  );
+}
+
+export default App;
