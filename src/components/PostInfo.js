576209a4ce374f053976fdb18287cd2b6117f2bb
+ <+>
2023-02-12 15:52:17 -0500

    Created a new component to hold the full Post info

---
+++ src/components/PostInfo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function PostInfo() {
+    return (
+        <div>
+            <p>this will hold the full post info</p>
+        </div>
+    )
+}
+
+
+export default PostInfo;
