8c16b8f0f3ef88d00925d3612b60b29258ed05b5
+ <+>
2023-02-13 08:17:48 -0500

    Finalized About

--- src/components/About.js
+++ src/components/About.js
@@ -2,10 +2,13 @@ import React from "react";
 
 function About() {
     return (
-        <div>
-            <h1>this is the about header</h1>
-            <h3>this is the about subtitle</h3>
-            <p>here is some random text inside of About</p>
+        <div className="textParentDiv">
+            <div className="textTextDiv">
+                <h1 className="h1Title">Why did I build MindLOG?</h1>
+                <h3>Welcome to my Phase 2 project.  I took inspiration from my own life and past experiences in overcoming tough stretches in life. Something that has worked for me consistently is extensive journaling. Over the years that has transitioned into short notes on particularly stressful days. I would frequently lose track of my notes and had no reliable way of seeing what trends were happening in my life. The app eliminates the need for the physical notes by taking a quick snapshot of what is going on in your day and displaying your history over time.</h3>
+                <br></br>
+                <h3>Eventually this project will be incorporated into a larger project with a more complete feature set.  I hope you enjoy the experience and I look forward to hearing what you think! </h3>
+            </div>
         </div>
     )
 }
